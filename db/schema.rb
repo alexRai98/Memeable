fd7e16034369db04eb1b60c898b73f2461fd24a0
gpanebra <gpanebramoina@gmail.com>
2020-05-28 14:16:52 -0500

    add vote model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_171138) do
+ActiveRecord::Schema.define(version: 2020_05_28_184701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "boby"
+    t.bigint "user_id", null: false
+    t.bigint "meme_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["meme_id"], name: "index_comments_on_meme_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "memes", force: :cascade do |t|
+    t.string "title"
+    t.string "type"
+    t.string "source"
+    t.integer "votes_count"
+    t.integer "comments_count"
+    t.bigint "category_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_memes_on_category_id"
+    t.index ["user_id"], name: "index_memes_on_user_id"
+  end
+
+  create_table "memes_tags", id: false, force: :cascade do |t|
+    t.bigint "meme_id", null: false
+    t.bigint "tag_id", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +70,8 @@ ActiveRecord::Schema.define(version: 2020_05_28_171138) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "memes"
+  add_foreign_key "comments", "users"
+  add_foreign_key "memes", "categories"
+  add_foreign_key "memes", "users"
 end
