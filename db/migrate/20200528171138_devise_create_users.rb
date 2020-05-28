b288c8ffe4d492d8957ebc4188d2ca51b2fa331b
gpanebra <gpanebramoina@gmail.com>
2020-05-28 14:07:56 -0500

    update

--- db/migrate/20200528171138_devise_create_users.rb
+++ db/migrate/20200528171138_devise_create_users.rb
@@ -32,7 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :username
+      t.string :username 
       t.integer :memes_count
 
       t.timestamps null: false
