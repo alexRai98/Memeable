ce0bd4fdb77b099b3db853cff0b33aab69ff8498
Rai <raikcuerda19@gmail.com>
2020-05-28 22:51:30 -0500

    create callback for user

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -3,6 +3,6 @@ class Meme < ApplicationRecord
   belongs_to :owner, foreign_key: :user_id , class_name: "User"
   has_and_belongs_to_many :tags
   has_many :votes
-  #has_many :voters, trough: :votes, source: :user
+  has_many :voters, trough: :votes, source: :user
   has_many :comments
 end
