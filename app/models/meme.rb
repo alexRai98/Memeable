189bdf0a9d6fa79d2edd0b60169f494595a725b6
saida brito <saidabrito57@gmail.com>
2020-05-29 12:47:31 -0500

    Added type validation

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -4,7 +4,7 @@ class Meme < ApplicationRecord
   belongs_to :owner , foreign_key: :user_id, class_name: "User"
   has_and_belongs_to_many :tags 
   has_many :votes
-
-  enum type: { image: 0, gif: 1}
-  validates :sources, presence: true
+  enum type: { image: "image", gif: "gif"}
+  validates :source, presence: true
+  validates :type, presence: true
 end
