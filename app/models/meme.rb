581782e641f4be7e4c6a391737419cb0afd4b573
saida brito <saidabrito57@gmail.com>
2020-05-29 13:11:23 -0500

    Added type and source validations

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -4,10 +4,14 @@ class Meme < ApplicationRecord
   belongs_to :owner, foreign_key: :user_id , class_name: "User", counter_cache: true
   has_and_belongs_to_many :tags
   has_many :votes
-  #has_many :voters, trough: :votes, source: :user
+  has_many :voters, through: :votes, source: :user
   has_many :comments
   validates :title, uniqueness: true, presence: true
   validates :source, presence: true
+  validates :type, presence: true
+  validates :source, format: { with: /\Ahttps:\/\/.+(.jpg|.png|.gif)\z/}
+
+  enum type: {image: "image", gif: "gif"}
 
   private
 
