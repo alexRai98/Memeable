c4929afe52306cd27f26a6895db14f33ba7b6458
Rai <raikcuerda19@gmail.com>
2020-05-28 22:21:15 -0500

    update models: meme,user,vote

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,6 +1,8 @@
 class Meme < ApplicationRecord
   belongs_to :category
-  belongs_to :owner , class_name: "User"
-  has_and_belongs_to_many :tag
+  belongs_to :owner, foreign_key: :user_id , class_name: "User"
+  has_and_belongs_to_many :tags
   has_many :votes
+  #has_many :voters, trough: :votes, source: :user
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :username, uniqueness: true, presence: true     
   has_many :votes 
+  has_many :meme
+  
+ 
 end
 
 
