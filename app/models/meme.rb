e9e3350219848e0ff42af460111b6436ba393666
saida brito <saidabrito57@gmail.com>
2020-05-28 22:25:21 -0500

    Added Home page view for visitors

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,6 +1,6 @@
 class Meme < ApplicationRecord
   belongs_to :category
-  belongs_to :owner , class_name: "User"
-  has_and_belongs_to_many :tag
+  belongs_to :owner , foreign_key: :user_id, class_name: "User"
+  has_and_belongs_to_many :tags 
   has_many :votes
 end
